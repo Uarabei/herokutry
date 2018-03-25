11beacb63d3613073a8b586f1c6ec7d7ecf7c1c7
Valeriy Samovarov <samovarov.val@gmail.com>
2018-03-25 15:14:43 +0600

    add project and speechclod

--- bot.py
+++ bot.py
@@ -20,6 +20,14 @@ def repeat_all_messages(message):
 def voice_processing(message):
     file_info = bot.get_file(message.voice.file_id)
     file = requests.get('https://api.telegram.org/file/bot{0}/{1}'.format(TOKEN, file_info.file_path))
+    try:
+        # обращение к нашему новому модулю
+        text = speech_to_text(bytes=file.content)
+        bot.send_message(message.chat.id, 'Распознал команду ' + text)
+    except SpeechException:
+        bot.send_message(message.chat.id, 'Извините, не удалось распознать запрос')
+    else:
+        bot.send_message(message.chat.id, 'Почему-то выпал в блок else!')
 
 if __name__ == '__main__':
      bot.polling(none_stop=True)
